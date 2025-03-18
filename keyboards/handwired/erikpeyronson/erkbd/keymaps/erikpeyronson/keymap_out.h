c67821a9584b0bfce8e442df20f0a14f8c6c6574
Erik Peyronson <erik.peyronson@gmail.com>
2025-03-18 07:12:57 +0100

    Remove uneccesary KC_TRANS

--- keyboards/handwired/erikpeyronson/erkbd/keymaps/erikpeyronson/keymap_out.h
+++ keyboards/handwired/erikpeyronson/erkbd/keymaps/erikpeyronson/keymap_out.h
@@ -29,9 +29,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [ETC] = LAYOUT_split_3x6_5(
         /********************************************************************************************************************************************************************************************************/
-        TG(SWE),         KC_TRNS,         KC_TRNS,         KC_TRNS,         KC_TRNS,         KC_TRNS,        /**/        KC_TRNS,          KC_TRNS,        KC_INSERT,       KC_TRNS,          KC_TRNS,        RALT(KC_W),
-        KC_CAPS,         KC_TRNS,         KC_TRNS,         KC_TRNS,         KC_TRNS,         KC_TRNS,        /**/        KC_TRNS,          KC_TRNS,        KC_TRNS,         KC_TRNS,          RALT(KC_P),     RALT(KC_Q),
-        KC_TRNS,         KC_TRNS,         KC_TRNS,         KC_TRNS,         KC_TRNS,         QK_BOOT,        /**/        KC_TRNS,          KC_TRNS,        KC_TRNS,         KC_TRNS,          KC_TRNS,        KC_TRNS,
+        TG(SWE),         KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_NO,          /**/        KC_NO,            KC_NO,          KC_INSERT,       KC_NO,            KC_NO,          RALT(KC_W),
+        KC_CAPS,         KC_NO,           KC_TRNS,         KC_TRNS,         KC_TRNS,         KC_NO,          /**/        KC_NO,            KC_TRNS,        KC_TRNS,         KC_TRNS,          RALT(KC_P),     RALT(KC_Q),
+        KC_NO,           KC_NO,           KC_NO,           KC_NO,           KC_NO,           QK_BOOT,        /**/        KC_NO,            KC_NO,          KC_NO,           KC_NO,            KC_NO,          KC_NO,
         /********************************************************************************************************************************************************************************************************/
                          KC_NO,           TG(NAV),         TG(SYM),         TG(NUM),         MO(ETC),        /**/        MO(ETC),          TG(NUM),        TG(SYM),         TG(NAV),          KC_NO
         /********************************************************************************************************************************************************************************************************/
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [STC] = LAYOUT_split_3x6_5(
         /********************************************************************************************************************************************************************************************************/
-        TG(1),           DF(0),           KC_TRNS,          KC_TRNS,        KC_TRNS,         KC_TRNS,        /**/        KC_TRNS,          KC_TRNS,        KC_INSERT,       KC_TRNS,          KC_TRNS,        KC_MINS,
-        KC_CAPS,         KC_TRNS,         KC_TRNS,          KC_TRNS,        KC_TRNS,         KC_TRNS,        /**/        KC_TRNS,          KC_TRNS,        KC_TRNS,         KC_TRNS,          KC_SCLN,        KC_QUOT,
-        KC_TRNS,         KC_TRNS,         KC_TRNS,          KC_TRNS,        KC_TRNS,         QK_BOOT,        /**/        KC_TRNS,          KC_TRNS,        KC_TRNS,         KC_TRNS,          KC_TRNS,        KC_TRNS,
+        TG(1),           DF(0),           KC_NO,            KC_NO,          KC_NO,           KC_NO,          /**/        KC_NO,            KC_NO,          KC_INSERT,       KC_NO,            KC_NO,          KC_MINS,
+        KC_CAPS,         KC_NO,           KC_TRNS,          KC_TRNS,        KC_TRNS,         KC_NO,          /**/        KC_NO,            KC_TRNS,        KC_TRNS,         KC_TRNS,          KC_SCLN,        KC_QUOT,
+        KC_NO,           KC_NO,           KC_NO,            KC_NO,          KC_NO,           QK_BOOT,        /**/        KC_NO,            KC_NO,          KC_NO,           KC_NO,            KC_NO,          KC_NO,
         /********************************************************************************************************************************************************************************************************/
                          KC_NO,           TG(NAV),          TG(SYM),        TG(NUM),         MO(ETC),        /**/        MO(ETC),          TG(NUM),        TG(SYM),         TG(NAV),          KC_NO
         /********************************************************************************************************************************************************************************************************/
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /********************************************************************************************************************************************************************************************************/
         KC_GRV,          KC_1,            KC_2,             KC_3,           KC_4,            KC_5,           /**/        KC_6,              KC_7,           KC_8,           KC_9,             KC_0,           KC_EQL,
         KC_TRNS,         KC_NO,           KC_TRNS,          KC_TRNS,        KC_TRNS,         KC_NO,          /**/        KC_MINS,           RCTL_T(KC_4),   RSFT_T(KC_5),   LALT_T(KC_6),     KC_PAST,        RGUI_T(KC_BSLS),
-        KC_TRNS,         KC_NO,           KC_NO,            KC_NO,          KC_NO,           KC_NO,          /**/        KC_PLUS,           KC_1,           KC_2,           KC_3,             KC_SLSH,        LT(ETC,KC_DOT),
+        KC_NO,           KC_NO,           KC_NO,            KC_NO,          KC_NO,           KC_NO,          /**/        KC_PLUS,           KC_1,           KC_2,           KC_3,             KC_SLSH,        LT(ETC,KC_DOT),
         /********************************************************************************************************************************************************************************************************/
                          KC_NO,           KC_TRNS,          KC_TRNS,        KC_TRNS,         MO(ETC),        /**/        MO(ETC),           KC_TRNS,        KC_TRNS,        KC_0,             KC_NO
         /********************************************************************************************************************************************************************************************************/
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /********************************************************************************************************************************************************************************************************/
         KC_GRV,       KC_EXLM,            KC_AT,            KC_HASH,        KC_DLR,          KC_PERC,        /**/        KC_CIRC,           KC_AMPR,        KC_ASTR,        KC_LPRN,          KC_RPRN,        KC_EQL,
         MY_CAPS_WORD, KC_NO,              KC_TRNS,          KC_TRNS,        KC_TRNS,         KC_NO,          /**/        KC_PIPE,           RCTL_T(KC_LT),  RSFT_T(KC_GT),  LALT_T(KC_LBRC),  KC_RBRC,        RGUI_T(KC_BSLS),
-        KC_TRNS,      KC_NO,              KC_NO,            KC_NO,          KC_NO,           KC_NO,          /**/        KC_TILD,           KC_SCLN,        KC_DQUO,        KC_LCBR,          KC_RCBR,        LT(ETC, KC_PIPE),
+        KC_NO,        KC_NO,              KC_NO,            KC_NO,          KC_NO,           KC_NO,          /**/        KC_TILD,           KC_SCLN,        KC_DQUO,        KC_LCBR,          KC_RCBR,        LT(ETC, KC_PIPE),
         /********************************************************************************************************************************************************************************************************/
                       KC_NO,              KC_TRNS,          KC_TRNS,        KC_TRNS,         MO(ETC),        /**/        MO(ETC),           KC_TRNS,        KC_TRNS,        KC_TRNS,          KC_NO
         /********************************************************************************************************************************************************************************************************/
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /********************************************************************************************************************************************************************************************************/
         KC_NO,        KC_F1,              KC_F2,            KC_F3,          KC_F4,          KC_F5,           /**/         KC_F6,            KC_F7,           KC_F8,         KC_F9,            KC_F10,         KC_F11,
         KC_TRNS,      KC_NO,              KC_TRNS,          KC_TRNS,        KC_TRNS,        KC_NO,           /**/         KC_LEFT,          RCTL_T(KC_DOWN), RSFT_T(KC_UP), LALT_T(KC_RGHT),  KC_NO,          RGUI_T(KC_F12),
-        KC_TRNS,      KC_NO,              KC_CUT,           KC_COPY,        KC_PSTE,        KC_NO,           /**/         KC_HOME,          KC_PGDN,         KC_PGUP,       KC_PGDN,          KC_END,         QK_AREP,
+        KC_NO,        KC_NO,              KC_CUT,           KC_COPY,        KC_PSTE,        KC_NO,           /**/         KC_HOME,          KC_PGDN,         KC_PGUP,       KC_PGDN,          KC_END,         QK_AREP,
         /********************************************************************************************************************************************************************************************************/
                       KC_NO,              KC_TRNS,          KC_TRNS,        KC_TRNS,        MO(ETC),         /**/         MO(ETC),          KC_TRNS,         KC_TRNS,       KC_TRNS,          KC_NO
         /********************************************************************************************************************************************************************************************************/
